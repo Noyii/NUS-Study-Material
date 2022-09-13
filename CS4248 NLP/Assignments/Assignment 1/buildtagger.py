39fee427c0a6c3691afa9302b54159af9532af57
Niharika Shrivastava <chunnushrivastava@gmail.com>
2022-09-14 03:57:10 +0800

    Add Assignment 1 solution for NLP

--- CS4248 NLP/Assignments/Assignment 1/buildtagger.py
+++ CS4248 NLP/Assignments/Assignment 1/buildtagger.py
@@ -4,10 +4,102 @@ import os
 import math
 import sys
 import datetime
+import json
+
+from collections import defaultdict, Counter
+
+# 1) Extract individual counts for tag bigram and tag word likelihoods in the training corpus.
+# 2) For the unknown words in the likelihood table, we change all words with single occurrence to UNK and use this for all unknwon words.
+# 3) The tag bigram is add one smoothed.
+# 4) Using the above, Viterbi is implemented on the HMM.
 
 
 def train_model(train_file, model_file):
-    # write your code here. You can add functions as well.
+    # Write your code here. You can add functions as well.
+    vocabulary = set()
+    states = set()
+    initial_tag_probabilities = Counter()
+    transition_probability = defaultdict(Counter)
+    emission_probability = defaultdict(Counter)
+    word_tag_frequency = defaultdict(Counter)
+    total_tag_count = 0
+
+    with open(train_file, 'r') as training:
+        for line in training.readlines():
+            line = line.strip().split(" ")
+            pre_tag = "RDM" # Random word
+            word, tag = None, None
+
+            for entry in line:
+                if entry.count('/') > 1: # E.g., 1/2-year/CD
+                    idx = entry[::-1].find('/')
+                    word = entry[: -idx-1]
+                    tag = entry[-idx:]
+                else:
+                    word, tag = entry.split('/')
+
+                vocabulary.add(word)
+                states.add(tag)
+
+                initial_tag_probabilities[tag] += 1
+                transition_probability[pre_tag][tag] += 1
+                emission_probability[tag][word] += 1
+                word_tag_frequency[word][tag] += 1
+
+                pre_tag = tag
+                total_tag_count += 1
+
+    distinct_tag_count = len(states) # 45 for Penn TreeBank
+    distinct_word_count = len(vocabulary)
+
+    for i in initial_tag_probabilities.keys():
+        initial_tag_probabilities[i] = math.log((initial_tag_probabilities[i] + 1) / (total_tag_count + distinct_tag_count))
+
+    for tag_transition_counter in transition_probability.values():
+        total = sum(tag_transition_counter.values())
+        for tag in tag_transition_counter.keys():
+            tag_transition_counter[tag] = math.log((tag_transition_counter[tag] + 1) / (total + len(tag_transition_counter)))
+
+    for word_emission_counter in emission_probability.values():
+        total = sum(word_emission_counter.values())
+        for word in word_emission_counter.keys():
+            word_emission_counter[word] = math.log((word_emission_counter[word] + 1) / (total + len(word_emission_counter)))
+        
+    emission_probability['RDM'] = {'RDM': math.log(1.0)}
+
+    with open(model_file, 'w') as model:
+        print("Writing vocabulary")
+        model.write(str(vocabulary))
+        model.write('\n')
+
+        print("Writing states")
+        model.write(str(states))
+        model.write('\n')
+
+        print("Writing initial state probabilities")
+        model.write(json.dumps(initial_tag_probabilities))
+        model.write('\n')
+
+        print("Writing transition probabilities")
+        model.write(json.dumps(transition_probability))
+        model.write('\n')
+
+        print("Writing emission probabilities")
+        model.write(json.dumps(emission_probability))
+        model.write('\n')
+
+        print("Writing word-tag frequencies")
+        model.write(json.dumps(word_tag_frequency))
+        model.write('\n')
+
+        print("Writing distinct states count")
+        model.write(json.dumps(distinct_tag_count))
+        model.write('\n')
+
+        print("Writing distinct word counts")
+        model.write(json.dumps(distinct_word_count))
+        model.write('\n')
+
     print('Finished...')
 
 if __name__ == "__main__":
--- CS4248 NLP/Assignments/Assignment 1/runtagger.py
+++ CS4248 NLP/Assignments/Assignment 1/runtagger.py
@@ -2,12 +2,102 @@
 
 import os
 import math
+import ast
 import sys
 import datetime
+import json
+
+PROBABILITY = "probability"
+BACKPOINTER = "backpointer"
+
+
+def viterbi(observation, vocabulary, states, \
+initial_state_probability, emission_probability, transition_probability, \
+word_tag_frequency):
+
+    distinct_tag_count = len(states)
+    distinct_word_count = len(vocabulary)
+
+    unknown_word_prob = math.log(1/distinct_word_count)
+    unknown_state_prob = math.log(1/distinct_tag_count)
+    
+    V = [{}]
+
+    for state in states:
+        V[0][state] = {
+            PROBABILITY: initial_state_probability[state] + \
+                                emission_probability[state].get(observation[0], unknown_word_prob),
+            BACKPOINTER: None
+        }
+    
+    for t in range(1, len(observation)):
+        V.append({})
+
+        for state in states:
+            probs = [(V[t-1][prev_state][PROBABILITY] + \
+                        transition_probability[prev_state].get(state, unknown_state_prob)) \
+                            for prev_state in states]
+            
+            max_transition_prob = max(probs)
+            max_emission_prob = max_transition_prob + emission_probability[state].get(observation[t], unknown_word_prob)
+            
+            for prev_state in states:
+                transition_prob = V[t-1][prev_state][PROBABILITY] + transition_probability[prev_state].get(state, unknown_state_prob)
+                if (max_transition_prob == transition_prob):
+                        V[t][state] = {PROBABILITY: max_emission_prob, BACKPOINTER: prev_state}
+
+    pre_tag = "RDM"
+    output = ""
+    tags = []
+
+    max_probability = max(stage[PROBABILITY] for stage in V[-1].values())
+    for state, stage in V[-1].items():
+        if stage[PROBABILITY] == max_probability:
+            tags.append(state)
+            pre_tag = state
+            break
+
+    for t in range(len(V) - 2, -1, -1):
+        x = V[t+1][pre_tag][BACKPOINTER]
+        tags.append(x)
+        pre_tag = x
+
+    tags = tags[::-1]
+
+    for i in range(len(observation)):
+        output += observation[i] + '/' + tags[i] + " " 
+
+    return output[:-1]
 
 
 def tag_sentence(test_file, model_file, out_file):
     # write your code here. You can add functions as well.
+    result = []
+
+    print("Reading model")
+    with open(model_file, 'r') as model:
+        lines = model.readlines()
+        
+        vocabulary = ast.literal_eval(lines[0])
+        states = ast.literal_eval(lines[1])
+        initial_state_probability = json.loads(lines[2])
+        transition_probability = json.loads(lines[3])
+        emission_probability = json.loads(lines[4])
+        word_tag_frequency = json.loads(lines[5])
+
+    print("Performing Viterbi algorithm")
+    with open(test_file, 'r') as test:
+        for line in test.readlines():
+            observation = line.strip().split(" ")
+            output = viterbi(observation, vocabulary, states, initial_state_probability, emission_probability, transition_probability, word_tag_frequency)
+            result.append(output)
+
+    print("Writing answer in out_file")
+    with open(out_file, 'w') as answer:
+        for line in result:
+            answer.write(line)
+            answer.write('\n')
+
     print('Finished...')
 
 if __name__ == "__main__":
